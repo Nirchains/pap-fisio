2ec8472210e5106ef01fb314309e1f6232e2ec57
Ezequiel <Ezequiel@Ezequiel-PC>
2019-06-04 13:10:51 +0200

    Se crea una nueva plantilla de listas para las páginas del PAP. El balance de creditos asignados al grupo aparecerá en rojo o azul

---
+++ logs/2019/error.php
@@ -0,0 +1,13 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2019-04-19 17:03:20 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2019-04-19T17:03:20+00:00	INFO ::1	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2019-04-19T17:03:47+00:00	INFO ::1	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2019-04-19T17:04:15+00:00	INFO ::1	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2019-05-12T16:36:36+00:00	INFO ::1	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2019-05-12T16:36:43+00:00	INFO ::1	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2019-05-28T10:18:52+00:00	INFO ::1	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
+2019-05-28T10:29:12+00:00	INFO ::1	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
---
+++ logs/2019/jcontroller.log.php
@@ -0,0 +1,14 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2019-06-03 09:50:15 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2019-06-03T09:50:15+00:00	INFO ::1	controller	Holding edit ID com_fabrik.edit.form.5 Array (     [0] => 5 ) 
+2019-06-03T09:50:31+00:00	INFO ::1	controller	Releasing edit ID com_fabrik.edit.form.5 Array ( ) 
+2019-06-03T09:54:09+00:00	INFO ::1	controller	Holding edit ID com_fabrik.edit.element.89 Array (     [0] => 89 ) 
+2019-06-03T09:57:10+00:00	INFO ::1	controller	Releasing edit ID com_fabrik.edit.element.89 Array ( ) 
+2019-06-03T09:59:26+00:00	INFO ::1	controller	Holding edit ID com_fabrik.edit.element.89 Array (     [0] => 89 ) 
+2019-06-04T10:40:04+00:00	INFO 127.0.0.1	controller	Holding edit ID com_fabrik.edit.list.9 Array (     [0] => 9 ) 
+2019-06-04T10:40:08+00:00	INFO 127.0.0.1	controller	Holding edit ID com_fabrik.edit.list.8 Array (     [0] => 8 ) 
+2019-06-04T10:41:50+00:00	INFO 127.0.0.1	controller	Releasing edit ID com_fabrik.edit.list.8 Array ( ) 
